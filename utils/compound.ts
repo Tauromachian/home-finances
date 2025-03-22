3cab7e1085d6819e634b7f1da46a9c087dd39e16
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-22 17:01:07 -0400

    Calculate rate on calculate compound function

--- utils/compound.ts
+++ utils/compound.ts
@@ -1,8 +1,10 @@
 export function calculateCompoundInterest(
   principal: number,
-  rate: number,
+  annualPercent: number,
   time: number = 10,
 ) {
+  const rate = annualPercent / 100;
+
   const amount = principal * Math.pow(1 + rate, time);
   const interest = amount - principal;
   return {
