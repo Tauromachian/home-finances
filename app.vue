2b0173751fcef1af2621e6962a99a5ccf06b9272
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-03-29 09:41:01 -0400

    Use color neutral

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <main class="font-nunito min-h-screen bg-gray-100">
+  <main class="font-nunito min-h-screen bg-neutral">
     <div class="max-w-7xl mx-4 lg:mx-auto pt-5">
       <NavBar class="mb-8"></NavBar>
 
