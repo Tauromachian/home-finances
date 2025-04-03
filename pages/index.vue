e6a7efb089347757cb9ca9ab7d63df6d25a568b0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-02 20:07:49 -0400

    Move stats card logic into index

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { useExpenseStore } from "../stores/expenses";
 
+import { categories } from "../utils/categories";
+
 const expenseStore = useExpenseStore();
 
 const appToaster = ref(null);
 const fadingInOutArrow = ref(false);
+const chartKey = ref(0);
+const frequencies = ref(["All", "One time", "Monthly", "Annual"]);
+const selectedExpenseType = ref("All");
 
 function scrollToStats() {
   const component = document.getElementById("expense-stats");
@@ -17,7 +22,6 @@ function scrollToStats() {
 }
 
 function submitForm(form) {
-  // show scroll-down arrow
   expenseStore.addExpense(form);
 
   if (!appToaster?.value) return;
@@ -28,6 +32,25 @@ function submitForm(form) {
 function closeScrollDown() {
   fadingInOutArrow.value = false;
 }
+
+function getCategory(category) {
+  return categories[
+    categories.findIndex((item) => {
+      return item.name == category;
+    })
+  ];
+}
+
+const filteredExpenses = computed(() => {
+  const allExpenses = expenseStore.expenses;
+
+  return allExpenses.filter((expense) => {
+    if (selectedExpenseType.value === "All") return true;
+    return expense.types === selectedExpenseType.value;
+  });
+});
+
+watch(filteredExpenses, () => chartKey.value++);
 
 onMounted(() => {
   expenseStore.loadExpenses();
@@ -41,7 +64,57 @@ onMounted(() => {
     <div class="flex flex-col md:flex-row gap-5 md:items-start">
       <ExpenseForm @submit="submitForm"></ExpenseForm>
 
-      <ExpenseStatsCard />
+      <div id="expense-stats" class="w-full">
+        <BaseButtonGroup v-model="selectedExpenseType" class="mb-4">
+          <BaseButton
+            v-for="(frequency, index) in frequencies"
+            :key="`expense-type-${index}`"
+            :value="frequency"
+          >
+            {{ frequency }}
+          </BaseButton>
+        </BaseButtonGroup>
+
+        <div class="flex flex-wrap gap-3">
+          <AppCard>
+            <div
+              v-if="!filteredExpenses?.length"
+              class="flex justify-center mt-6"
+            >
+              No expenses! Add one
+            </div>
+
+            <AppCardBody>
+              <ExpenseDonutChart
+                v-if="filteredExpenses?.length"
+                :key="chartKey"
+                :expenses="filteredExpenses"
+                :categories="categories"
+              ></ExpenseDonutChart>
+            </AppCardBody>
+          </AppCard>
+
+          <AppCard>
+            <div
+              v-if="!filteredExpenses?.length"
+              class="flex justify-center mt-6"
+            >
+              No expenses! Add one
+            </div>
+
+            <AppCardBody>
+              <ExpenseItem
+                v-for="expense in filteredExpenses"
+                :key="expense.id"
+                :expense="expense"
+                :category="getCategory(expense.categories)"
+                class="border-b last:border-0 border-gray-300"
+                @remove="expenseStore.removeExpense(expense.id)"
+              ></ExpenseItem>
+            </AppCardBody>
+          </AppCard>
+        </div>
+      </div>
     </div>
 
     <AppToaster ref="appToaster"></AppToaster>
