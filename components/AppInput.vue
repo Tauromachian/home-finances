8feb4aa4a78cc3b5d77b01a50687a17bf1ff003b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-05 09:23:56 -0400

    Remove unnecessary class

--- components/AppInput.vue
+++ components/AppInput.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     <label :for="$attrs.name">{{ label }}</label>
     <div class="mt-1 mb-5 text-gray-800 relative">
       <slot name="prepend"></slot>
-      <Field v-bind="$attrs" class="text-field peer" :class="inputClass" />
+      <Field v-bind="$attrs" class="text-field" :class="inputClass" />
       <ErrorText> {{ error }}</ErrorText>
     </div>
   </div>
