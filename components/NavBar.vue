2761c7dc70388c882142dafbf143dfe28d74c6f7
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2025-04-05 10:51:47 -0400

    Use text to indicate opened view instead of position

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -9,22 +9,16 @@ const links = [
     to: "/compound-calculator",
   },
 ];
-
-const route = useRoute();
 </script>
 
 <template>
   <nav class="bg-teal-50 py-4 px-8 rounded-full drop-shadow-md">
     <ul class="font-medium flex gap-5">
       <li v-for="link in links" :key="link.to" class="me-2">
-        <NuxtLink :to="link.to" class="relative">
+        <NuxtLink :to="link.to" class="relative" active-class="text-teal-700">
           <span>
             {{ link.name }}
           </span>
-          <span
-            v-show="route.path === link.to"
-            class="absolute bg-primary-500 rounded-full h-1 w-2/3 -bottom-2 right-0"
-          ></span>
         </NuxtLink>
       </li>
     </ul>
