e334a7db78b53f75e229b8b0b0d26c0d79a1b9c0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-17 10:28:02 +0100

    Remove border of navbar

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -12,7 +12,7 @@ const links = [
 </script>
 
 <template>
-  <nav class="bg-teal-50 py-4 px-8 rounded-full drop-shadow-md">
+  <nav class="bg-teal-50 py-4 px-8 drop-shadow-md">
     <ul class="font-medium flex gap-5">
       <li v-for="link in links" :key="link.to" class="me-2">
         <NuxtLink :to="link.to" class="relative" active-class="text-teal-700">
