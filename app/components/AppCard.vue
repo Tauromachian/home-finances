5c02674115cda93157b43b4296f42674026abd8f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-20 18:03:22 +0100

    Change card styles

--- app/components/AppCard.vue
+++ app/components/AppCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white w-full max-w-md h-min drop-shadow-md">
+  <div class="shadow-lg bg-neutral-2 w-full max-w-md h-min rounded-2xl">
     <slot></slot>
   </div>
 </template>
