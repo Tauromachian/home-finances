84cc9b5c1f8f727fe648814a327931ce7af072c7
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-21 10:48:33 +0100

    Allow cards to expand

--- app/components/AppCard.vue
+++ app/components/AppCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="shadow-sm bg-neutral-2 w-full max-w-md h-min rounded-2xl">
+  <div class="shadow-sm bg-neutral-2 w-full h-min rounded-2xl">
     <slot></slot>
   </div>
 </template>
