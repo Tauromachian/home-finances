a4a83034a4da27970bd73ad345af1c228072996c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-21 09:47:28 +0100

    Use text colors on NavBar

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -13,12 +13,12 @@ const links = [
 
 <template>
   <nav class="bg-neutral-1 rounded-xl p-1">
-    <ul class="font-medium text-sm flex justify-evenly gap-5">
+    <ul class="font-medium text-sm flex justify-evenly gap-5 text-text-1">
       <li v-for="link in links" :key="link.to" class="w-full text-center">
         <NuxtLink
           :to="link.to"
           class="block me-2 py-2 px-8 w-full rounded-lg"
-          active-class="bg-neutral-2"
+          active-class="bg-neutral-2 text-text-0"
         >
           <span>
             {{ link.name }}
