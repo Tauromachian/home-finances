247f74833bfb8b3d06d491de158df7fda83935ca
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-21 11:38:55 +0100

    Make basic category component

---
+++ app/components/expense/Category.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { Expense } from "~/types/expense";
+
+defineProps({
+  expenses: {
+    type: Array as PropType<Expense[]>,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <AppCard>
+    <AppCardBody>
+      <p class="uppercase text-sm font-bold">By Category</p>
+    </AppCardBody>
+  </AppCard>
+</template>
+
+<style scoped></style>
