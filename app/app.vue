10b3947c39c729f980df0bed553a9782c15e4eae
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-23 08:16:23 +0100

    Show switch to change theme

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 const appToaster = useTemplateRef("appToaster");
 
+const theme = ref(false);
+
+watch(theme, (value) => {
+  const html = document.querySelector("html");
+  html.setAttribute("data-theme", value ? "dark" : "light");
+});
+
 provide("appToaster", appToaster);
 </script>
 
 <template>
   <main class="font-sans min-h-screen bg-neutral-0 text-text-0">
-    <header class="max-w-7xl mx-4 lg:mx-auto pt-5">
+    <header class="flex items-center max-w-7xl mx-4 lg:mx-auto pt-5">
       <h1 class="font-serif text-4xl font-bold text-text-1">
         Home
         <span class="text-accent-0 italic"> Finances </span>
       </h1>
+
+      <AppSwitch v-model="theme" class="ml-auto"></AppSwitch>
     </header>
 
     <div class="max-w-7xl mx-4 lg:mx-auto pt-5">
