7867331fe75824014d364e62a0af8331061df3e1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-23 19:35:32 +0100

    Diminish available width space for app

--- app/app.vue
+++ app/app.vue
@@ -22,7 +22,7 @@ provide("appToaster", appToaster);
 
 <template>
   <main class="font-sans min-h-screen bg-neutral-0 text-text-0">
-    <header class="flex items-center max-w-7xl mx-4 lg:mx-auto pt-5">
+    <header class="flex items-center max-w-6xl mx-4 lg:mx-auto pt-5">
       <h1 class="font-serif text-4xl font-bold text-text-1">
         Home
         <span class="text-accent-0 italic"> Finances </span>
@@ -31,7 +31,7 @@ provide("appToaster", appToaster);
       <AppSwitch v-model="theme" class="ml-auto" :label="themeName"></AppSwitch>
     </header>
 
-    <div class="max-w-7xl mx-4 lg:mx-auto pt-5">
+    <div class="max-w-6xl mx-4 lg:mx-auto pt-5">
       <NavBar class="mb-8"></NavBar>
 
       <NuxtPage />
