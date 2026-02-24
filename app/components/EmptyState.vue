d909a8cf9cbb9e6bcb415951e78a8480b33892aa
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-24 21:36:11 +0100

    Implement EmptyState component

---
+++ app/components/EmptyState.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center gap-5 justify-center my-6">
+    <Icon size="48" name="material-symbols:note-outline"></Icon>
+
+    <p>No expenses! Add one</p>
+  </div>
+</template>
