9ef551f9f6f6d43e3100e2c0d36c38edc6ed2899
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-24 20:49:31 +0100

    Make holdings and Item components

---
+++ app/components/investment/Holdings.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { Category } from "~/types/category";
+import type { Investment } from "~/types/investment";
+
+function getCategory(category: string): Category {
+  return assets.find((item) => {
+    return item.name == category;
+  });
+}
+
+defineProps<{ investments: Investment[] }>();
+</script>
+
+<template>
+  <AppCard>
+    <AppCardBody>
+      <InvestmentItem
+        v-for="(investment, index) in investments"
+        :key="`investment-${index}`"
+        :investment
+        :category="getCategory(investment.category)"
+      ></InvestmentItem>
+    </AppCardBody>
+  </AppCard>
+</template>
+
+<style scoped></style>
---
+++ app/components/investment/Item.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type { Category } from "~/types/category";
+import type { Investment } from "~/types/investment";
+
+defineProps<{ investment: Investment; category: Category }>();
+defineEmits(["remove"]);
+
+const isConfirmationDialogOpen = ref(false);
+</script>
+
+<template>
+  <AppCard class="rounded-xl" variant="outlined">
+    <AppCardBody class="flex items-center py-2">
+      <div
+        class="min-w-12 w-12 h-12 mr-3 flex justify-center items-center rounded-md"
+        :style="{ background: category.color + '12' }"
+      >
+        <Icon
+          :size="22"
+          :name="category.icon"
+          :style="{ color: category.color }"
+        />
+      </div>
+
+      <div class="w-full flex items-center">
+        <div class="flex flex-col text-text-1 justify-center">
+          <span class="font-bold">{{ investment.name }}</span>
+          <p
+            v-if="investment.description && investment.description !== ''"
+            class="text-sm mt-2"
+          >
+            {{ investment.description }}
+          </p>
+        </div>
+
+        <p class="ml-auto rounded-full text-white px-2 text-sm uppercase mr-3">
+          {{ investment.category }}
+        </p>
+
+        <p class="font-serif text-text-1 mr-2 text-lg">
+          €{{ investment.currentValue }}
+        </p>
+
+        <BaseButton
+          variant="text"
+          class="flex items-center"
+          @click="isConfirmationDialogOpen = true"
+        >
+          <Icon
+            name="material-symbols:delete-outline"
+            class="w-5 h-5 text-red-700 hover:text-red-800 transition-all duration-100 ease-in-out"
+          />
+        </BaseButton>
+      </div>
+    </AppCardBody>
+
+    <DialogConfirmDelete
+      v-model="isConfirmationDialogOpen"
+      @click:delete="$emit('remove')"
+    ></DialogConfirmDelete>
+  </AppCard>
+</template>
