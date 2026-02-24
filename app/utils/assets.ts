aa35b563c0a2b34f8d78c8b0e16dd77d1915a1ec
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-24 16:38:57 +0100

    Make investments assets

---
+++ app/utils/assets.ts
@@ -0,0 +1,19 @@
+import type { Category } from "~/types/category";
+
+export const assets: Category[] = [
+  {
+    icon: "material-symbols:work",
+    color: "#2563eb",
+    name: "Stocks/ETFs",
+  },
+  {
+    icon: "material-symbols:currency-bitcoin",
+    color: "#2563eb",
+    name: "Crypto",
+  },
+  {
+    icon: "material-symbols:money-bag-rounded",
+    color: "#2563eb",
+    name: "Savings",
+  },
+];
