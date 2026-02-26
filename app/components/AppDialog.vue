9d145a45a8e86de0c53fd6a119c17872627a4949
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-02-26 20:44:15 +0100

    Ref/ Simplify Dialog with popover

--- app/components/AppDialog.vue
+++ app/components/AppDialog.vue
@@ -1,58 +1,48 @@
 <script setup lang="ts">
 const model = defineModel<boolean>();
 
-const dialogEl = useTemplateRef("dialog");
-
 const DURATION = 150; // milliseconds
-
-watch(model, (val) => {
-  if (val) {
-    dialogEl.value.showModal();
-  } else {
-    setTimeout(() => {
-      dialogEl.value.close();
-    }, DURATION);
-  }
-});
-
 const classes = computed(() => {
   return model.value
     ? ["scale-100", "backdrop:opacity-40"]
     : ["scale-0", "backdrop:opacity-0"];
 });
 
-const escapeEvent = (event: KeyboardEvent) => {
-  if (!model.value) return;
+const dialogID = useId();
+const dialogEl = useTemplateRef("dialog");
 
-  if (event.key === "Escape") closeModal();
-};
+let hasJustOpened = false;
 
-function closeModal() {
+watch(model, (val) => {
+  hasJustOpened = true;
+  if (val) dialogEl.value.togglePopover();
+});
+
+function onToggle() {
+  if (hasJustOpened) {
+    hasJustOpened = false;
+    return;
+  }
+
   model.value = false;
 }
-
-onMounted(() => {
-  document.addEventListener("keydown", escapeEvent);
-});
-
-onUnmounted(() => {
-  document.removeEventListener("keydown", escapeEvent);
-});
 </script>
 
 <template>
   <Teleport to="body">
     <dialog
+      :id="dialogID"
       ref="dialog"
+      popover
       class="backdrop:bg-black w-sm rounded-2xl left-1/2 top-1/2 -translate-1/2 transition bg-neutral-0"
       :class="[...classes, `duration-${DURATION}`]"
-      @click="closeModal"
+      @toggle="onToggle"
     >
-      <div @click.stop>
+      <div>
         <button
+          :popovertarget="dialogID"
           class="absolute z-10 top-2 cursor-pointer right-4 text-gray-600 hover:text-red-500 text-lg font-bold"
           type="button"
-          @click="closeModal"
         >
           ✕
         </button>
