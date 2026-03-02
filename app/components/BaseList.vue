53d50aac2ab0882a2fdeb4cc0d3eb3a6a20cd3e5
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-02 22:26:43 +0100

    Set list color

--- app/components/BaseList.vue
+++ app/components/BaseList.vue
@@ -3,11 +3,11 @@ defineProps<{ items: { name: string; id: string | number }[] }>();
 </script>
 
 <template>
-  <ul class="py-3">
+  <ul class="py-3 bg-neutral-2 rounded-2xl">
     <li
       v-for="(item, index) in items"
       :key="`${index}-${item.id}`"
-      class="px-3 py-2 cursor-pointer hover:bg-neutral-1"
+      class="px-3 py-2 cursor-pointer hover:bg-neutral-1 text-text-0"
     >
       {{ item.name }}
     </li>
