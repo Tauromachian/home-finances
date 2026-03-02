7cd951f2ab3131a2172214852fcb0c9b34c61634
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-02 09:11:11 +0100

    Use text danger instead of text red

--- app/components/ErrorText.vue
+++ app/components/ErrorText.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="text-red-500 text-sm empty:opacity-0 opacity-100 transition">
+  <div class="text-danger-0 text-sm empty:opacity-0 opacity-100 transition">
     <slot></slot>
   </div>
 </template>
