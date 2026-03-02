09c69c46214863a72447fc3d6182748b20d9dc8c
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-02 10:17:27 +0100

    Test ErrorTest component

---
+++ test/nuxt/ErrorText.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
+
+import ErrorText from "../../app/components/ErrorText.vue";
+
+describe("ErrorText.vue component", () => {
+  it("Sets display none if it has no slots", () => {
+    const wrapper = mount(ErrorText, {});
+
+    expect(wrapper.element.style.display).toBe("none");
+  });
+
+  it("Sets display block if it has slots", () => {
+    const wrapper = mount(ErrorText, {
+      slots: { default: "Test text" },
+    });
+
+    expect(wrapper.element.style.display).toBe("");
+  });
+});
