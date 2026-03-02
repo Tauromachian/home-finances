60b8cb73736ed95df527bf5bf3540ce7c3e0babe
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-02 13:58:24 +0100

    Ref/ Move transition to main css file

--- app/components/AppAutocomplete.vue
+++ app/components/AppAutocomplete.vue
@@ -149,15 +149,3 @@ onUnmounted(() => {
     </span>
   </div>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.06s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
