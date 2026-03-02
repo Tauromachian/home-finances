1892159e5d0e8a382a5429201e43a421f430f849
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-02 20:46:39 +0100

    Make basic list component

---
+++ app/components/BaseList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ items: { name: string; id: string | number }[] }>();
+</script>
+
+<template>
+  <ul class="py-3">
+    <li
+      v-for="(item, index) in items"
+      :key="`${index}-${item.id}`"
+      class="px-3 py-2 cursor-pointer hover:bg-neutral-1"
+    >
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
