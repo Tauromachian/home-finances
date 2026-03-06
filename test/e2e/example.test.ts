5f3631161bb23d1699e3b88c71d93f3548f10384
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-06 09:54:22 +0100

    Install playwright

--- test/e2e/example.test.ts
+++ test/e2e/example.test.ts
@@ -1,11 +1,20 @@
-import { describe, expect, it } from 'vitest'
-import { $fetch, setup } from '@nuxt/test-utils/e2e'
+import { test, expect } from "@playwright/test";
 
-describe('example e2e test', async () => {
-  await setup()
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
-  it('renders the index page', async () => {
-    const html = await $fetch('/')
-    expect(html).toContain('Nuxt')
-  })
-})
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "Installation" }),
+  ).toBeVisible();
+});
