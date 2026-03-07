d6c7acd12ae99b9bb2f92830b6d86fb539cc87fa
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-03-07 20:25:01 +0100

    Add edit and delete expense to the flow

--- test/e2e/expenses.test.ts
+++ test/e2e/expenses.test.ts
@@ -1,19 +1,30 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
-test("Insert flow", async ({ page }) => {
+test("Insert/Edit and Delete expense flow", async ({ page }) => {
   await page.goto("https://homefinances.jose-garcia.net/");
-
   await page.getByRole("button", { name: "+ Add Expense" }).click();
   await page.locator('input[name="name"]').click();
-  await page.locator('input[name="name"]').fill("Test name");
-  await page.locator('input[name="name"]').press("Tab");
-  await page.getByRole("spinbutton").fill("10");
+  await page.locator('input[name="name"]').fill("Test expense");
+  await page.getByRole("spinbutton").click();
+  await page.getByRole("spinbutton").press("ArrowUp");
+  await page.getByRole("spinbutton").fill("1");
+  await page.getByRole("spinbutton").press("ArrowUp");
+  await page.getByRole("spinbutton").fill("2");
+  await page.getByRole("spinbutton").press("ArrowUp");
+  await page.getByRole("spinbutton").fill("20");
   await page.locator('input[name="frequency"]').click();
-  await page.getByRole("dialog").getByText("Monthly").click();
+  await page.getByText("Annual").click();
   await page.locator('input[name="category"]').click();
-  await page.getByText("Travel").click();
+  await page.getByText("Food").click();
   await page.getByRole("button", { name: "Add Expense", exact: true }).click();
-  await page.getByRole("button").nth(3).click();
+  await page.getByTestId("expenses-items").getByRole("button").click();
 
-  await expect(page.getByTestId("expenses-items")).toContainText("Test name");
+  await page.getByRole("button", { name: "Edit" }).click();
+  await page.locator('input[name="name"]').click();
+  await page.locator('input[name="name"]').fill("Test expense 123");
+  await page.getByRole("button", { name: "Add Expense", exact: true }).click();
+  await page.getByTestId("expenses-items").getByRole("button").click();
+
+  await page.getByRole("button", { name: "Delete" }).click();
+  await page.locator("button").filter({ hasText: "Delete" }).click();
 });
