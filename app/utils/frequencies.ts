b7f50d34d2a6ae4f522dfb853ddc4e25f76959e0
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-04-06 20:26:41 +0200

    Put frequencies in a util file

---
+++ app/utils/frequencies.ts
@@ -0,0 +1,11 @@
+export const frequencies = [
+  { title: "One Time", value: "One Time" },
+  {
+    title: "Monthly",
+    value: "Monthly",
+  },
+  {
+    title: "Annual",
+    value: "Annual",
+  },
+];
