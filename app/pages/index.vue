31245481b6bddb83fc3a7bdb7483cdad75c0ec1b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-04-11 22:39:16 +0200

    Make dashboard view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,227 +1,42 @@
-<script setup lang="ts">
-import { expensesCategories } from "~/utils/categories";
-
-import type { Expense } from "~/types/expense";
-
-import { useExpenseStore } from "../stores/expenses";
-
-type Frequency = "All" | "One time" | "Monthly" | "Annual";
-type FormMode = "edit" | "insert";
-
-const expenseStore = useExpenseStore();
-
-const formRef = useTemplateRef("formRef");
-
-const appToaster = inject<Ref>("appToaster");
-
-const expenseForm = ref<Partial<Expense>>();
-const selectedExpenseType = ref<Frequency>("All");
-const isOpen = ref(false);
-const isConfirmationDialogOpen = ref(false);
-
-const EMPTY_EXPENSE: Expense = {
-  name: "",
-  amount: 0,
-  category: "",
-  frequency: "",
-  description: "",
-};
-
-let selectedId: number | string = "";
-let formMode: FormMode;
-
-function showMessage(message: string) {
-  isOpen.value = false;
-
-  if (!appToaster?.value) return;
-
-  appToaster.value.openToast(message);
-}
-
-function submitForm(form: Expense) {
-  if (formMode === "insert") {
-    expenseStore.addExpense(form);
-    showMessage("New expense added!");
-  } else {
-    expenseStore.editExpense(selectedId, form);
-    showMessage("Expense edited");
-  }
-}
-
-function openForm(mode: FormMode, id?: string | number) {
-  formMode = mode;
-
-  if (mode === "insert") {
-    expenseForm.value = { ...EMPTY_EXPENSE };
-
-    formRef.value.internalRef.resetForm();
-  } else {
-    const expenseToEdit: Expense = expenseStore.expenses.find(
-      (expense: Expense) => expense.id === id,
-    );
-
-    selectedId = expenseToEdit.id;
-    expenseForm.value = { ...expenseToEdit };
-  }
-
-  isOpen.value = true;
-}
-
-function openDeleteConfirmationDialog(id: string | number) {
-  selectedId = id;
-  isConfirmationDialogOpen.value = true;
-}
-
-function deleteExpense() {
-  expenseStore.removeExpense(selectedId);
-  isConfirmationDialogOpen.value = false;
-}
-
-const filteredExpenses = computed<Expense[]>(() => {
-  const allExpenses = expenseStore.expenses;
-
-  return allExpenses.filter((expense) => {
-    if (selectedExpenseType.value === "All") return true;
-    return expense.types === selectedExpenseType.value;
-  });
-});
-
-const totalExpenses = computed(() => {
-  return filteredExpenses.value.reduce((acum: number, next: Expense) => {
-    acum += Number(next.amount);
-
-    return acum;
-  }, 0);
-});
-
-const monthlyExpenses = computed(() => {
-  return filteredExpenses.value.reduce((acum: number, next: Expense) => {
-    if (next.frequency === "Monthly") {
-      const monthly = (next.amount / 12).toFixed(2);
-      acum += Number(monthly);
-    } else {
-      acum += next.amount;
-    }
-
-    return acum;
-  }, 0);
-});
-
-const categoriesCount = computed(() => {
-  const categoriesObj = filteredExpenses.value.reduce(
-    (acum: Record<string, boolean>, next: Expense) => {
-      acum[next.category] = true;
-      return acum;
-    },
-    {},
-  );
-
-  return Object.keys(categoriesObj).length;
-});
-
-onMounted(() => expenseStore.loadExpenses());
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <BaseButton class="mb-5" @click="openForm('insert')">
-      <span class="text-xl">+</span> Add Expense
-    </BaseButton>
+  <div class="flex flex-col gap-4">
+    <AppCard color="accent-3" class="text-text-inverse">
+      <AppCardBody>
+        <p class="text-text-0">Net Worth</p>
+        <h2 class="text-5xl font-serif text-accent-4">€10 000</h2>
+        <p class="text-text-0">Portfolio value plus Income minus Expenses</p>
+      </AppCardBody>
+    </AppCard>
 
-    <div class="grid gap-5">
-      <div class="grid md:grid-cols-3 gap-5">
-        <AppCard>
-          <AppCardBody>
-            <p class="text-sm">Total Expenses</p>
-            <p class="text-3xl font-serif text-accent-0 mt-2">
-              €{{ totalExpenses }}
-            </p>
-          </AppCardBody>
-        </AppCard>
-        <AppCard>
-          <AppCardBody>
-            <p class="text-sm">Monthly</p>
-            <p class="text-3xl font-serif mt-2 text-text-1">
-              €{{ monthlyExpenses }}
-            </p>
-          </AppCardBody>
-        </AppCard>
-        <AppCard>
-          <AppCardBody>
-            <p class="text-sm">Categories</p>
-            <p class="text-3xl font-serif mt-2 text-text-1">
-              {{ categoriesCount }}
-            </p>
-          </AppCardBody>
-        </AppCard>
-      </div>
-      <div class="grid md:grid-cols-2 gap-5 max-h-120">
-        <AppCard class="h-full max-h-[inherit]">
-          <AppCardBody>
-            <p class="text-md font-bold">Breakdown</p>
-          </AppCardBody>
-
-          <div
-            v-if="!filteredExpenses?.length"
-            class="flex flex-col items-center gap-5 justify-center my-6"
-          >
-            <Icon size="48" name="material-symbols-light:note-outline"></Icon>
-
-            <p>No expenses! Add one</p>
-          </div>
-
-          <AppCardBody>
-            <ExpenseDonutChart
-              v-if="filteredExpenses?.length"
-              :expenses="filteredExpenses"
-              :categories="expensesCategories"
-            ></ExpenseDonutChart>
-          </AppCardBody>
-        </AppCard>
-
-        <AppCard class="h-full max-h-[inherit]">
-          <AppCardBody class="max-h-full">
-            <p class="text-md font-bold mb-4">Expenses</p>
-            <div
-              class="flex flex-col gap-3 max-h-96 overflow-y-scroll"
-              data-testid="expenses-items"
-            >
-              <ExpenseItem
-                v-for="expense in filteredExpenses"
-                :key="expense.id"
-                :expense="expense"
-                variant="outlined"
-                :category="
-                  getCategoryByName(expense.category, expensesCategories)
-                "
-                @delete="openDeleteConfirmationDialog"
-                @edit="(id: string | number) => openForm('edit', id)"
-              ></ExpenseItem>
-            </div>
-
-            <div
-              v-if="!filteredExpenses?.length"
-              class="flex flex-col items-center gap-5 justify-center my-6"
-            >
-              <Icon size="48" name="material-symbols-light:note-outline"></Icon>
-              <p>No expenses! Add one</p>
-            </div>
-          </AppCardBody>
-        </AppCard>
-      </div>
+    <div class="grid grid-cols-4 gap-3">
+      <AppCard>
+        <AppCardBody>
+          <p class="text-sm">Balance change</p>
+          <p class="text-3xl font-serif text-accent-0">3000</p>
+        </AppCardBody>
+      </AppCard>
+      <AppCard>
+        <AppCardBody>
+          <p class="text-sm">Income</p>
+          <p class="text-3xl font-serif text-accent-0">10000</p>
+        </AppCardBody>
+      </AppCard>
+      <AppCard>
+        <AppCardBody>
+          <p class="text-sm">Expenses</p>
+          <p class="text-3xl font-serif text-accent-0">10000</p>
+        </AppCardBody>
+      </AppCard>
+      <AppCard>
+        <AppCardBody>
+          <p class="text-sm">Cashflow</p>
+          <p class="text-3xl font-serif text-accent-0">10000</p>
+        </AppCardBody>
+      </AppCard>
     </div>
 
-    <DialogConfirmDelete
-      v-model="isConfirmationDialogOpen"
-      @click:delete="deleteExpense"
-    ></DialogConfirmDelete>
-
-    <AppDialog v-model="isOpen">
-      <ExpenseForm
-        ref="formRef"
-        v-model="expenseForm"
-        @submit="submitForm"
-      ></ExpenseForm>
-    </AppDialog>
+    <DashboardColumnChart></DashboardColumnChart>
   </div>
 </template>
