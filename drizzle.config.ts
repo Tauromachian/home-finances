7c64db41352ce5e8506653024c17b427d4c45a3f
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-05-25 21:40:20 +0200

    Add DB variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+
+const DB_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 export default defineConfig({
   out: "./server/drizzle",
   schema: "./server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DB_URL!,
+    url: DB_URL!,
   },
 });
--- server/orm/index.ts
+++ server/orm/index.ts
@@ -1,4 +1,8 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DB_URL!);
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+
+const DB_URL = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+export const db = drizzle(DB_URL);
