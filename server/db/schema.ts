6c250bfa9fd30c7f429cd3d0cceb03a5f40318a9
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-06-18 22:23:00 +0200

    Get number values as numbers from DB

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -2,7 +2,7 @@ import { integer, pgTable, varchar, numeric } from "drizzle-orm/pg-core";
 
 export const expensesTable = pgTable("expenses", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  amount: numeric().notNull(),
+  amount: numeric({ mode: "number" }).notNull(),
   name: varchar({ length: 255 }).notNull(),
   category: varchar({ length: 255 }).notNull(),
   frequency: varchar({ length: 255 }).notNull().unique(),
@@ -13,7 +13,7 @@ export const investmentsTable = pgTable("investments", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   category: varchar({ length: 255 }).notNull(),
-  amount: numeric().notNull(),
-  currentValue: numeric().notNull(),
+  amount: numeric({ mode: "number" }).notNull(),
+  currentValue: numeric({ mode: "number" }).notNull(),
   description: varchar({ length: 255 }),
 });
