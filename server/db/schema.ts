6f2bd6e1f8ac1c473cafce9dc5cd25719646bfeb
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-06-20 14:17:08 +0200

    Add relationship with Supabase's auth

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,7 +1,26 @@
-import { integer, pgTable, varchar, numeric } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  numeric,
+  text,
+  pgSchema,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+const auth = pgSchema("auth");
+
+export const usersTable = auth.table("users", {
+  id: uuid("id").primaryKey(),
+  fullName: text("full_name"),
+  phone: varchar("phone", { length: 256 }),
+});
 
 export const expensesTable = pgTable("expenses", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id),
   amount: numeric({ mode: "number" }).notNull(),
   name: varchar({ length: 255 }).notNull(),
   category: varchar({ length: 255 }).notNull(),
@@ -11,6 +30,9 @@ export const expensesTable = pgTable("expenses", {
 
 export const investmentsTable = pgTable("investments", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id),
   name: varchar({ length: 255 }).notNull(),
   category: varchar({ length: 255 }).notNull(),
   amount: numeric({ mode: "number" }).notNull(),
