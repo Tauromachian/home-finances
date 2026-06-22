feab125ea06ed3e0ad07d5507d57877159a5d013
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-06-22 08:45:33 +0200

    Add guest user and password at login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -18,15 +18,24 @@ const signInWithOtp = async () => {
 </script>
 
 <template>
-  <AppCard class="max-w-96 mb-auto mx-auto">
-    <form class="card-body" @submit.prevent="signInWithOtp">
-      <AppInput v-model="form.email" type="email" label="Email" />
-      <AppInput v-model="form.password" type="password" label="Password" />
-      <div class="flex w-full">
-        <BaseButton class="ml-auto" type="submit">
-          Sign In with E-Mail
-        </BaseButton>
-      </div>
-    </form>
-  </AppCard>
+  <div class="max-w-96 mb-auto mx-auto">
+    <AppCard class="mb-5">
+      <form class="card-body" @submit.prevent="signInWithOtp">
+        <AppInput v-model="form.email" type="email" label="Email" />
+        <AppInput v-model="form.password" type="password" label="Password" />
+        <div class="flex w-full">
+          <BaseButton class="ml-auto" type="submit">
+            Sign In with E-Mail
+          </BaseButton>
+        </div>
+      </form>
+    </AppCard>
+
+    <AppCard>
+      <AppCardBody>
+        <p>Guest User: test@test.com</p>
+        <p>Guest Password: 123</p>
+      </AppCardBody>
+    </AppCard>
+  </div>
 </template>
