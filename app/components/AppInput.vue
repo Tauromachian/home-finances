ccc477c19bd71470daabd044e346a0a4eda50031
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-01 08:31:23 +0200

    Change prepend and append styles when events are declared

--- app/components/AppInput.vue
+++ app/components/AppInput.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   label: { type: String, default: "" },
   error: { type: String, default: "" },
   inputClass: { type: String, default: "" },
   noError: { type: Boolean, default: false },
+  "onClick:prepend": { type: Function, default: undefined },
+  "onClick:append": { type: Function, default: undefined },
 });
 
 const attrs = useAttrs();
@@ -34,14 +36,22 @@ const name = computed<string>(() => {
     <div class="mt-1 relative" :class="{ 'mb-5': !noError, 'mb-1': noError }">
       <div
         v-if="$slots.prepend"
-        class="bg-neutral-1 text-text-1 absolute inset-y-0 left-0 px-2 flex items-center pointer-events-none rounded-l-xl border border-neutral-1"
+        class="bg-neutral-1 text-text-1 absolute inset-y-0 left-0 px-2 flex items-center rounded-l-xl border border-neutral-1"
+        :class="{
+          'pointer-events-none': !props['onClick:prepend'],
+          'cursor-pointer': props['onClick:prepend'],
+        }"
         @click="emit('click:prepend')"
       >
         <slot name="prepend"></slot>
       </div>
       <div
         v-if="$slots.append"
-        class="bg-neutral-1 text-text-1 absolute inset-y-0 right-0 px-2 flex items-center pointer-events-none rounded-r-xl border border-neutral-1"
+        class="bg-neutral-1 text-text-1 absolute inset-y-0 right-0 px-2 flex items-center rounded-r-xl border border-neutral-1"
+        :class="{
+          'pointer-events-none': !props['onClick:append'],
+          'cursor-pointer': props['onClick:append'],
+        }"
         @click="emit('click:append')"
       >
         <slot name="append"></slot>
