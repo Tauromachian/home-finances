180e3242e70c1dec28513e876ec8855b0b6560a6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-04 21:09:26 +0200

    Use variables from runtime config to conform the DB_URL

--- server/orm/index.ts
+++ server/orm/index.ts
@@ -3,6 +3,8 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 const config = useRuntimeConfig();
 
-const DB_URL = config.DB_URL;
+const { dbUser, dbPassword, dbHost, dbPort, dbName } = config;
+
+const DB_URL = `postgresql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
 
 export const db = drizzle(DB_URL as string);
