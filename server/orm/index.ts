6518fe0b8d18788852eaf4d4164199a0cee0abc8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-05 10:05:43 +0200

    Remove console

--- server/orm/index.ts
+++ server/orm/index.ts
@@ -7,6 +7,4 @@ const { dbUser, dbPassword, dbHost, dbPort, dbName } = config;
 
 const DB_URL = `postgresql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
 
-console.log("DB_URL", DB_URL);
-
 export const db = drizzle(DB_URL as string);
