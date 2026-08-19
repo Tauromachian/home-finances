5958a8cb8658be37450f439bbafb9e2d98e8e439
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-19 09:17:09 +0200

    Make basic unit test for AppInput file

---
+++ test/nuxt/AppInput.test.ts
@@ -0,0 +1,16 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect } from "vitest";
+import AppInput from "~/components/AppInput.vue";
+
+describe("AppInput", () => {
+  it("renders label", () => {
+    const wrapper = mount(AppInput, { props: { label: "Test label" } });
+
+    expect(wrapper.html()).toContain("Test label");
+  });
+  it("hides label if label props is not passed", () => {
+    const wrapper = mount(AppInput);
+
+    expect(wrapper.find("label")).to.not.toBeFalsy();
+  });
+});
