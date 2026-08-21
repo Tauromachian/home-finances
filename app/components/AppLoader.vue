d9f54af45b24991e25874f49bb7da2d8eb001ad6
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-21 13:51:04 +0200

    Add size to AppLoader component

--- app/components/AppLoader.vue
+++ app/components/AppLoader.vue
@@ -1,8 +1,15 @@
+<script setup lang="ts">
+const { size = "18" } = defineProps<{
+  size?: number | string;
+}>();
+</script>
+
 <template>
-  <div class="flex items-center justify-center p-8">
+  <div class="flex items-center justify-center">
     <Icon
       name="material-symbols-light:progress-activity"
-      class="animate-spin text-2xl"
+      class="animate-spin size-24"
+      :size
     />
   </div>
 </template>
