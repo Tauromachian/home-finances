c5b44aa98de9cfd56f0c6ae419ea9b6f904debf8
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-21 13:35:50 +0200

    Make AppLoader component

---
+++ app/components/AppLoader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center justify-center p-8">
+    <Icon
+      name="material-symbols-light:progress-activity"
+      class="animate-spin text-2xl"
+    />
+  </div>
+</template>
