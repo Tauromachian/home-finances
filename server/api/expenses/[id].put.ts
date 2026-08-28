0d3877a11ccf65c549a204119136d748afd1af3b
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-08-28 19:09:22 +0200

    Get expense id from route

--- server/api/expenses/[id].put.ts
+++ server/api/expenses/[id].put.ts
@@ -5,15 +5,13 @@ import { db } from "@@/server/orm";
 import { expensesTable } from "@@/server/db/schema";
 
 export default defineEventHandler(async (event) => {
+  const id = Number(event.context.params?.id);
   const user = event.context.user;
 
   const expense = await readBody(event);
   expense.userId = user.id;
 
-  await db
-    .update(expensesTable)
-    .set(expense)
-    .where(eq(expensesTable.id, expense.id));
+  await db.update(expensesTable).set(expense).where(eq(expensesTable.id, id));
 
   return { msg: "Success" };
 });
