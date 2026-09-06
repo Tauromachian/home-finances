022d76b34a9004756140d00915287bc7da46a3b1
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-06 21:09:18 +0200

    Add more theme helpers

--- app/types/theme.ts
+++ app/types/theme.ts
@@ -1,1 +1,11 @@
-export type ThemeName = "light" | "system" | "dark";
+export const themes = ["light", "system", "dark"] as const;
+
+export function ensureTheme(possibleTheme: string): ThemeName {
+  if ((themes as readonly string[]).includes(possibleTheme)) {
+    return possibleTheme as ThemeName;
+  }
+
+  return "system";
+}
+
+export type ThemeName = (typeof themes)[number];
