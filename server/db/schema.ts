0c72f9235599d2868085fa8f327615e0129fa890
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-08 21:03:36 +0200

    Remove dates from expenses

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -7,7 +7,6 @@ import {
   pgSchema,
   uuid,
   pgEnum,
-  date,
 } from "drizzle-orm/pg-core";
 
 import { Frequency } from "../../app/types/frequency";
@@ -37,8 +36,6 @@ export const expensesTable = pgTable("expenses", {
   category: varchar({ length: 255 }).notNull(),
   frequency: frequencyEnum(),
   description: varchar({ length: 255 }),
-  startDate: date("start_date"),
-  endDate: date("end_date"),
 });
 
 export const investmentsTable = pgTable("investments", {
