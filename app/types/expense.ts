ac7d49724bcada1268635e9ebd645611b0355d03
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-10 09:42:46 +0200

    Separate Expense type from ProgrammedExpense type

--- app/types/expense.ts
+++ app/types/expense.ts
@@ -5,6 +5,17 @@ export type Expense = {
   name: string;
   amount: number;
   category: string;
+  date: string;
+  description: string;
+};
+
+export type ProgrammedExpense = {
+  id?: string | number;
+  name: string;
+  amount: number;
+  category: string;
   frequency: Frequency;
   description: string;
+  chargeDay: number | null;
+  chargeMonth: number | null;
 };
