425d470c0d54ab32832c0dd125d5e5d2ceaf145a
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-15 15:47:27 +0200

    Update income types

--- app/types/income.ts
+++ app/types/income.ts
@@ -1,7 +1,19 @@
+import type { Frequency } from "./frequency";
+
 export type Income = {
   id?: string | number;
   name: string;
   amount: number;
-  frequency: string;
+  incomeDate: string;
   description: string;
+};
+
+export type ProgrammedIncome = {
+  id?: string | number;
+  name: string;
+  amount: number;
+  frequency: Frequency;
+  description: string;
+  chargeDay: number | null;
+  chargeMonth: number | null;
 };
