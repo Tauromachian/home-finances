9149e15f3b60ca869ccad7f4a9164c90a690f706
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-17 09:00:31 +0200

    Make user groups schema

--- server/db/schema/users-groups.ts
+++ server/db/schema/users-groups.ts
@@ -1,14 +1,20 @@
-import { integer, pgTable, uuid } from "drizzle-orm/pg-core";
+import { integer, pgTable, unique, uuid } from "drizzle-orm/pg-core";
 
 import { groupsTable } from "./groups";
 import { usersTable } from "./users";
 
-export const usersGroupsTable = pgTable("users_groups", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  userId: uuid("user_id")
-    .notNull()
-    .references(() => usersTable.id),
-  groupId: integer("group_id")
-    .notNull()
-    .references(() => groupsTable.id),
-});
+export const usersGroupsTable = pgTable(
+  "users_groups",
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => usersTable.id),
+    groupId: integer("group_id")
+      .notNull()
+      .references(() => groupsTable.id),
+  },
+  (table) => [
+    unique("users_groups_user_group_unique").on(table.userId, table.groupId),
+  ],
+);
