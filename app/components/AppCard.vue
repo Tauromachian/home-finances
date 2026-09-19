3c39c0ccdd4a2b6eed670c3e320d33c642ab8a4d
Jose Garcia <54786476+Tauromachian@users.noreply.github.com>
2026-09-19 22:21:05 +0200

    Add radius prop to AppCard

--- app/components/AppCard.vue
+++ app/components/AppCard.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-const { variant = "", color = "neutral-2" } = defineProps<{
+const {
+  variant = "",
+  color = "neutral-2",
+  radius = "rounded-2xl",
+} = defineProps<{
   variant?: "" | "outlined";
   color?: string;
+  radius?: string;
 }>();
 
 const classes = computed(() => {
@@ -10,12 +15,16 @@ const classes = computed(() => {
     outlined: ["border", "border-neutral-1", "bg-neutral-0"],
   };
 
-  return classesByVariant[variant];
+  const classByVariant = classesByVariant[variant];
+
+  classByVariant.push(radius);
+
+  return classByVariant;
 });
 </script>
 
 <template>
-  <div class="w-full rounded-2xl" :class="classes">
+  <div class="w-full" :class="classes">
     <slot></slot>
   </div>
 </template>
